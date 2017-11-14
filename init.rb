940440fd9375df96c767c892ace0117acceead46
milos <milos@easy.cz>
2017-11-14 20:05:00 +0100

    Init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :pirate_login do
+  name 'Pirate Login plugin'
+  author 'Miloš Vlach'
+  description 'Removes common fields from login - so everyone uses OpenID.'
+  version '0.0.1'
+  url 'http://redmine.pirati.cz'
+  author_url 'http://vlach.me'
+end
